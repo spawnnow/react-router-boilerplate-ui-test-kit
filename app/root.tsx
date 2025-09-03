d1a99730903dd4e77c5b7cb72e69d5867c053d70
Spawn <157455266+spawnnow@users.noreply.github.com>
2025-09-03 15:47:40 +0530

    tv4

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,8 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import stylesheet from "./app.css?url";
+
+import "./styles/globals.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
